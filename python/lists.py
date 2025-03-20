fc593c82ec1bbeaa0dd3530fb2b8e9f8f10dd6d4
deepika-vd <152467057+deepika-vd@users.noreply.github.com>
2025-03-20 11:39:48 +0530

    Add files via upload
    Learned Python syntax and basic programming concepts.

---
+++ python/lists.py
@@ -0,0 +1,11 @@
+names = ["Harry", "Ron", "Hermione"]
+# Print the entire list:
+print(names)
+# Print the second element of the list:
+print(names[1])
+# Add a new name to the list:
+names.append("Draco")
+# Sort the list:
+names.sort()
+# Print the new list:
+print(names)
---
+++ python/pgm2_fuctions.py
@@ -0,0 +1,3 @@
+from fuctions import square
+for i in range(10):
+    print(f"square of{i} is {square(i)}")
