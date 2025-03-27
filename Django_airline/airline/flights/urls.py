37aa353d777554b443968e5abd2d8bd2452872c6
deepika-vd <152467057+deepika-vd@users.noreply.github.com>
2025-03-27 11:51:53 +0530

    Add files via upload

--- Django_airline/airline/flights/urls.py
+++ Django_airline/airline/flights/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
      path('', views.index, name="index"),
      path('airport/',views.index_airline, name="index_airport"),
-     path("<int:flight_id>", views.flight, name="flight")
-    
+     path("<int:flight_id>", views.flight, name="flight"),
+     path("<int:flight_id>/book/", views.book, name="book"),
 ]
